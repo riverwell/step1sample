9299eb811c31bb8b0ceefc8b95a58b2393fe9530
Yuta Kawai <yuta.kawai@nextremer.com>
2016-12-09 16:19:06 +0900

    [change]チュートリアル2までの変更

--- birthday.js
+++ birthday.js
@@ -26,7 +26,7 @@ function calculateBirthday() {
   var birth = getBirthday();
 
   var diff = getDateDiff(birth, now);
-  var message = "あなたが生まれてから" + diff.days + "日になりました";
+  var message = "あなたが生まれてから" + diff.days + "日になりました！";
 
   var result = document.getElementById("result");
   result.textContent = message;
